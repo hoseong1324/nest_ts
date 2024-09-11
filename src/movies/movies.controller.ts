fd37496627baf72b84d79278b34418f91b752c93
이호성 <hoseong1324@naver.com>
2024-09-11 17:19:43 +0900

    모듈 분할 및 DTO 활용

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,12 +1,14 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
 import { getDefaultAutoSelectFamily } from 'net';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
 
-    constructor(readonly moviesService: MoviesService){}
+    constructor(private readonly moviesService: MoviesService){}
 
     @Get()
     getAll() :Movie[] {
@@ -14,23 +16,24 @@ export class MoviesController {
     }
 
     @Get("/:id")
-    getOne(@Param('id') id: string) : Movie{
+    getOne(@Param('id') id: number) : Movie{
         return this.moviesService.getOne(id);
     }
 
     @Post()
-    create(@Body() movieData){
+    create(@Body() movieData : CreateMovieDto){
         return this.moviesService.create(movieData);
         
     }
 
     @Delete("/:id")
-    remove(@Param('id') id : string){
+    remove(@Param('id') id : number){
         return this.moviesService.deleteOne(id);
     }
 
     @Patch("/:id")
-    patch(@Param('id') id : string, @Body() updateData){
+    patch(@Param('id') id : number, @Body() updateData : UpdateMovieDto){
+        this.moviesService.update(id,updateData);
         return {
             updatedMovie: id,
             updateData,
